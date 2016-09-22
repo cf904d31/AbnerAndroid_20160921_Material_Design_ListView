bd206e574c515a80a419d782a5e6c8cf8262ce17
cf904d31 <cf904d31@gmail.com>
2016-09-22 09:40:00 +0800

    material design android

--- app/src/main/java/iii/org/tw/material_design_listview/List2Activity.java
+++ app/src/main/java/iii/org/tw/material_design_listview/List2Activity.java
@@ -2,7 +2,10 @@ package iii.org.tw.material_design_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.LinkedList;
 
@@ -10,12 +13,24 @@ public class List2Activity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private String[] dataset = {"Item 1","Item 2","Item 3","Item 4","Item 5","Item 6","Item 7"};
     private LinkedList<String> data;
+    private LinearLayoutManager mLayoutManager;
+    private MyAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list2);
         recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter();
+        recyclerView.setAdapter(mAdapter);
+
+
         data = new LinkedList<>();
 
         for (String s : dataset) {
@@ -23,5 +38,32 @@ public class List2Activity extends AppCompatActivity {
         }
 
 
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+
+            public MyViewHolder(View itemView) {
+                super(itemView);
+            }
+        }
+
+//        public MyAdapter() {
+//        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(MyAdapter.MyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
